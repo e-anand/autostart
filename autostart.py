1abf48f0828798725a34bd37303975f1d896749e
E.ANAND <84004847+e-anand@users.noreply.github.com>
2023-08-26 22:04:04 +0530

    Update autostart.py

--- autostart.py
+++ autostart.py
@@ -37,25 +37,3 @@ if __name__ == "__main__":
                 restart_process(process_path)
             time.sleep(.1)
 
-def start_process(process_path):
-    try:
-        subprocess.Popen(process_path)
-        print(f"Started {process_path}")
-    except Exception as e:
-        print(f"Error starting {process_path}: {e}")
-
-process_name = "Moneymaker.exe"  # Update with the actual process name
-process_path = r"C:\path\to\Moneymaker.exe"  # Update with the actual path to the executable
-
-
-while True:
-    current_time = datetime.datetime.now().time()
-    
-    if start_time <= current_time <= end_time:
-        if not is_process_running(process_name):
-            start_process(process_path)
-    else:
-        print("Outside of monitoring time window.")
-        break  # Exit the loop if outside the time window
-    
-    time.sleep(1)  # Wait for 1 second before checking again
